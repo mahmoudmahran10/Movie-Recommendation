d89768a93b4307bf57639f4835c648850fce3245
mahmoudmahran <mahmoudmahran10@hotmail.com>
2024-10-12 14:12:12 +0300

    initial commit

---
+++ src/Components/Home/Anime.jsx
@@ -0,0 +1,251 @@
+import React from "react";
+import "./anime.css";
+import image1 from "./1.jpg";
+import image2 from "./2.jpg";
+import image3 from "./3.jpg";
+import image4 from "./4.jpg";
+import image5 from "./5.jpg";
+import image6 from "./6.jpg";
+import image7 from "./7.jpg";
+import image8 from "./8.jpg";
+
+const Anime = () => {
+  const movies = [
+    {
+      title: "Spellbound",
+      year: "2024",
+      duration: "109m",
+      type: "Movie",
+      badge: "sd",
+      image: image1,
+    },
+
+    {
+      title: "Everybody Still Hates Chris",
+      year: "SS 1",
+      duration: "EPS 2",
+      type: "TV",
+      badge: "hd",
+      image: image2,
+    },
+    {
+      title: "Twilight of the Gods",
+      year: "SS 1",
+      duration: "EPS 8",
+      type: "TV",
+      badge: "hd",
+      image: image3,
+    },
+    {
+      title: "The Wild Robot",
+      year: "2024",
+      duration: "102m",
+      type: "Movie",
+      badge: "cam",
+      image: image4,
+    },
+    {
+      title: "Transformers One",
+      year: "2024",
+      duration: "104m",
+      type: "Movie",
+      badge: "cam",
+      image: image5,
+    },
+    {
+      title: "Transformers One",
+      year: "2024",
+      duration: "104m",
+      type: "Movie",
+      badge: "hd",
+      image: image6,
+    },
+    {
+      title: "Transformers One",
+      year: "2024",
+      duration: "104m",
+      type: "Movie",
+      badge: "hd",
+      image: image7,
+    },
+    {
+      title: "Transformers One",
+      year: "2024",
+      duration: "104m",
+      type: "Movie",
+      badge: "hd",
+      image: image8,
+    },
+    {
+      title: "Transformers One",
+      year: "2024",
+      duration: "104m",
+      type: "Movie",
+      badge: "hd",
+      image: image8,
+    },
+    {
+      title: "Transformers One",
+      year: "2024",
+      duration: "104m",
+      type: "Movie",
+      badge: "hd",
+      image: image8,
+    },
+    {
+      title: "Transformers One",
+      year: "2024",
+      duration: "104m",
+      type: "Movie",
+      badge: "hd",
+      image: image8,
+    },
+    {
+      title: "Transformers One",
+      year: "2024",
+      duration: "104m",
+      type: "Movie",
+      badge: "hd",
+      image: image8,
+    },
+    {
+      title: "Transformers One",
+      year: "2024",
+      duration: "104m",
+      type: "Movie",
+      badge: "hd",
+      image: image8,
+    },
+    {
+      title: "Transformers One",
+      year: "2024",
+      duration: "104m",
+      type: "Movie",
+      badge: "hd",
+      image: image8,
+    },
+    {
+      title: "Transformers One",
+      year: "2024",
+      duration: "104m",
+      type: "Movie",
+      badge: "hd",
+      image: image8,
+    },
+    {
+      title: "Transformers One",
+      year: "2024",
+      duration: "104m",
+      type: "Movie",
+      badge: "hd",
+      image: image8,
+    },
+    {
+      title: "Transformers One",
+      year: "2024",
+      duration: "104m",
+      type: "Movie",
+      badge: "hd",
+      image: image8,
+    },
+    {
+      title: "Transformers One",
+      year: "2024",
+      duration: "104m",
+      type: "Movie",
+      badge: "hd",
+      image: image8,
+    },
+    {
+      title: "Transformers One",
+      year: "2024",
+      duration: "104m",
+      type: "Movie",
+      badge: "hd",
+      image: image8,
+    },
+    {
+      title: "Transformers One",
+      year: "2024",
+      duration: "104m",
+      type: "Movie",
+      badge: "hd",
+      image: image8,
+    },
+    {
+      title: "Transformers One",
+      year: "2024",
+      duration: "104m",
+      type: "Movie",
+      badge: "hd",
+      image: image8,
+    },
+    {
+      title: "Transformers One",
+      year: "2024",
+      duration: "104m",
+      type: "Movie",
+      badge: "hd",
+      image: image8,
+    },
+    {
+      title: "Transformers One",
+      year: "2024",
+      duration: "104m",
+      type: "Movie",
+      badge: "hd",
+      image: image8,
+    },
+    {
+      title: "Transformers One",
+      year: "2024",
+      duration: "104m",
+      type: "Movie",
+      badge: "hd",
+      image: image8,
+    },
+    // Add more movies as needed
+  ];
+
+  return (
+    <div>
+<h2 className="animeh2">Popular Animation</h2>
+
+    <div className="container">
+      
+      
+            {/* <div className="pagination">
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>→</button>
+                <button>»</button>
+            </div> */}
+
+      <div className="grid">
+        {movies.map((movie, index) => (
+          <div className="card" key={index}>
+            <div className={`badge ${movie.badge}`}>
+              {movie.badge.toUpperCase()}
+            </div>
+            <img src={movie.image} alt={movie.title} />
+            <p className="title">{movie.title}</p>
+            <p className="info">{`${movie.year} · ${movie.duration}`}</p>
+            <p className="type">{movie.type}</p>
+          </div>
+        ))}
+      </div>
+
+      {/* <div className="pagination">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>→</button>
+        <button>»</button>
+      </div> */}
+    </div>
+    </div>
+  );
+};
+
+export default Anime;
---
+++ src/Components/Home/Series.jsx
@@ -0,0 +1,241 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Series.css';
+
+function Series() {
+  return (
+    <div>
+      <h2>Popular Series</h2>
+      
+      {/* <button className="filter-button">Filter</button> */}
+
+      <div className="series-grid">
+        <Link to="/series/breaking-bad" className="series-card">
+          <img src="https://picsum.photos/200/300" alt="Series 1" />
+          <div className="series-info">
+            <h3>Breaking Bad</h3>
+            <p>Genre: Crime, Drama</p>
+            <p>Rating: 9.5/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/the-office" className="series-card">
+          <img src="https://picsum.photos/200/301" alt="Series 2" />
+          <div className="series-info">
+            <h3>The Office</h3>
+            <p>Genre: Comedy</p>
+            <p>Rating: 8.8/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/stranger-things" className="series-card">
+          <img src="https://picsum.photos/200/302" alt="Series 3" />
+          <div className="series-info">
+            <h3>Stranger Things</h3>
+            <p>Genre: Sci-Fi, Horror</p>
+            <p>Rating: 8.7/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/the-walking-dead" className="series-card">
+          <img src="https://picsum.photos/200/303" alt="Series 4" />
+          <div className="series-info">
+            <h3>The Walking Dead</h3>
+            <p>Genre: Horror, Drama</p>
+            <p>Rating: 8.2/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/game-of-thrones" className="series-card">
+          <img src="https://picsum.photos/200/304" alt="Series 5" />
+          <div className="series-info">
+            <h3>Game of Thrones</h3>
+            <p>Genre: Fantasy, Drama</p>
+            <p>Rating: 9.2/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/the-big-bang-theory" className="series-card">
+          <img src="https://picsum.photos/200/305" alt="Series 6" />
+          <div className="series-info">
+            <h3>The Big Bang Theory</h3>
+            <p>Genre: Comedy</p>
+            <p>Rating: 8.1/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/friends" className="series-card">
+          <img src="https://picsum.photos/200/306" alt="Series 7" />
+          <div className="series-info">
+            <h3>Friends</h3>
+            <p>Genre: Comedy</p>
+            <p>Rating: 8.9/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/the-100" className="series-card">
+          <img src="https://picsum.photos/200/307" alt="Series 8" />
+          <div className="series-info">
+            <h3>The 100</h3>
+            <p>Genre: Sci-Fi, Drama</p>
+            <p>Rating: 8.4/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/the-100" className="series-card">
+          <img src="https://picsum.photos/200/307" alt="Series 8" />
+          <div className="series-info">
+            <h3>The 100</h3>
+            <p>Genre: Sci-Fi, Drama</p>
+            <p>Rating: 8.4/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/the-100" className="series-card">
+          <img src="https://picsum.photos/200/307" alt="Series 8" />
+          <div className="series-info">
+            <h3>The 100</h3>
+            <p>Genre: Sci-Fi, Drama</p>
+            <p>Rating: 8.4/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/the-100" className="series-card">
+          <img src="https://picsum.photos/200/307" alt="Series 8" />
+          <div className="series-info">
+            <h3>The 100</h3>
+            <p>Genre: Sci-Fi, Drama</p>
+            <p>Rating: 8.4/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/the-100" className="series-card">
+          <img src="https://picsum.photos/200/307" alt="Series 8" />
+          <div className="series-info">
+            <h3>The 100</h3>
+            <p>Genre: Sci-Fi, Drama</p>
+            <p>Rating: 8.4/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/the-100" className="series-card">
+          <img src="https://picsum.photos/200/307" alt="Series 8" />
+          <div className="series-info">
+            <h3>The 100</h3>
+            <p>Genre: Sci-Fi, Drama</p>
+            <p>Rating: 8.4/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/the-100" className="series-card">
+          <img src="https://picsum.photos/200/307" alt="Series 8" />
+          <div className="series-info">
+            <h3>The 100</h3>
+            <p>Genre: Sci-Fi, Drama</p>
+            <p>Rating: 8.4/10</p>
+          </div>
+        </Link> 
+
+        <Link to="/series/the-100" className="series-card">
+          <img src="https://picsum.photos/200/307" alt="Series 8" />
+          <div className="series-info">
+            <h3>The 100</h3>
+            <p>Genre: Sci-Fi, Drama</p>
+            <p>Rating: 8.4/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/the-100" className="series-card">
+          <img src="https://picsum.photos/200/307" alt="Series 8" />
+          <div className="series-info">
+            <h3>The 100</h3>
+            <p>Genre: Sci-Fi, Drama</p>
+            <p>Rating: 8.4/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/the-100" className="series-card">
+          <img src="https://picsum.photos/200/307" alt="Series 8" />
+          <div className="series-info">
+            <h3>The 100</h3>
+            <p>Genre: Sci-Fi, Drama</p>
+            <p>Rating: 8.4/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/the-100" className="series-card">
+          <img src="https://picsum.photos/200/307" alt="Series 8" />
+          <div className="series-info">
+            <h3>The 100</h3>
+            <p>Genre: Sci-Fi, Drama</p>
+            <p>Rating: 8.4/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/the-100" className="series-card">
+          <img src="https://picsum.photos/200/307" alt="Series 8" />
+          <div className="series-info">
+            <h3>The 100</h3>
+            <p>Genre: Sci-Fi, Drama</p>
+            <p>Rating: 8.4/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/the-100" className="series-card">
+          <img src="https://picsum.photos/200/307" alt="Series 8" />
+          <div className="series-info">
+            <h3>The 100</h3>
+            <p>Genre: Sci-Fi, Drama</p>
+            <p>Rating: 8.4/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/the-100" className="series-card">
+          <img src="https://picsum.photos/200/307" alt="Series 8" />
+          <div className="series-info">
+            <h3>The 100</h3>
+            <p>Genre: Sci-Fi, Drama</p>
+            <p>Rating: 8.4/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/the-100" className="series-card">
+          <img src="https://picsum.photos/200/307" alt="Series 8" />
+          <div className="series-info">
+            <h3>The 100</h3>
+            <p>Genre: Sci-Fi, Drama</p>
+            <p>Rating: 8.4/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/the-100" className="series-card">
+          <img src="https://picsum.photos/200/307" alt="Series 8" />
+          <div className="series-info">
+            <h3>The 100</h3>
+            <p>Genre: Sci-Fi, Drama</p>
+            <p>Rating: 8.4/10</p>
+          </div>
+        </Link>
+
+        <Link to="/series/the-100" className="series-card">
+          <img src="https://picsum.photos/200/307" alt="Series 8" />
+          <div className="series-info">
+            <h3>The 100</h3>
+            <p>Genre: Sci-Fi, Drama</p>
+            <p>Rating: 8.4/10</p>
+          </div>
+        </Link>
+      </div>
+
+      {/* <div className="pagination">
+        <a href="#">1</a>
+        <a href="#">2</a>
+        <a href="#">3</a>
+        <a href="#">→</a>
+        <a href="#">»</a>
+      </div> */}
+    </div>
+  );
+}
+
+export default Series;
